865aa4fe8012d008dba730b8310020ac8b17af36
Faris Bagas Pratama <Neocrmencer@gmail.com>
2024-06-12 03:23:51 +0700

    Membuat fitur Login & logout

---
+++ app/src/main/java/org/d3if0024/assesmentmobpro3/model/User.kt
@@ -0,0 +1,8 @@
+package org.d3if0024.assesmentmobpro3.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+
+)
--- app/src/main/java/org/d3if0024/assesmentmobpro3/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0024/assesmentmobpro3/ui/screen/MainScreen.kt
@@ -5,23 +5,45 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.d3if0024.assesmentmobpro3.R
+import org.d3if0024.assesmentmobpro3.model.User
+import org.d3if0024.assesmentmobpro3.network.UserDataStore
+import org.d3if0024.assesmentmobpro3.network.signIn
+import org.d3if0024.assesmentmobpro3.network.signOut
 import org.d3if0024.assesmentmobpro3.ui.theme.AssesmentMobpro3Theme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+    val dataStore = UserDataStore(context)
+    val user by dataStore.userFlow.collectAsState(User())
+
+    var showDialog by remember { mutableStateOf(false) }
+
     Scaffold (
         topBar = {
             TopAppBar(title = {
@@ -30,11 +52,36 @@ fun MainScreen() {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                ),
+                actions = {
+                    IconButton(onClick = {
+                        if (user.email.isEmpty()){
+                            CoroutineScope(Dispatchers.IO) .launch { signIn(context, dataStore) }
+                        }
+                        else{
+                            showDialog = true
+                        }
+                    }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_person_24),
+                            contentDescription = stringResource(id = R.string.profil),
+                            tint = MaterialTheme.colorScheme.primary
+
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
         ScreenContent(Modifier.padding(padding))
+        if (showDialog){
+            ProfilDialog(
+                user = user ,
+                onDismissRequest = {showDialog = false }) {
+                CoroutineScope(Dispatchers.IO).launch { signOut(context, dataStore) }
+                showDialog = false
+            }
+        }
     }
 
 }
